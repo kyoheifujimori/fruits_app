074e6979eabd4d1c498f5bd168c14abfdd358b3c
kyoheifujimori <kyohei.fujimori@g.dreamcareer.co.jp>
2024-05-17 18:36:35 +0900

    add delete fuction. complete delete function.

--- src/Fruits.js
+++ src/Fruits.js
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './Fruits.css';
 
 function Fruits() {
-  // dataには何が入る？
-  // setDataの呼び出し箇所に注目（※１）
-  const [data, setData] = useState(/* ここに初期値。無しだとundefinedになる */);
+  // data変数の宣言
+  const [data, setData] = useState();
 
   // webページが読み込まれた後に処理を開始する
   useEffect(() => {
     console.log("call useEffect START");
     fetch('http://localhost:8080/fruits').then(response => {
       response.json().then(value => {
-        // ※２
         console.log(value);
+        // dataをdata変数にセットする
         setData(value);
       })
-      // catchを入れることで、サーバに接続できなくなったときに画面にエラーを出す代わりにコンソールに出す
     })
+      // エラーをキャッチしたときの処理
       .catch(error => {
         console.log(error);
         setData([]);
@@ -26,9 +25,8 @@ function Fruits() {
     return () => { };
   }, []);
 
-  // ----------------------------追加処理---------------------------------
 
-  // フルーツデータを再取得する関数
+  // フルーツデータを再取得する関数 -> 追加・削除処理共通なので外に置いとく
   const fetchFruitData = () => {
     fetch('http://localhost:8080/fruits')
       .then(response => response.json())
@@ -41,18 +39,19 @@ function Fruits() {
       });
   }
 
+  // ----------------------------追加処理---------------------------------
+
   // フォームから送信された際の処理
   // 送信ボタンが押された時に行われる処理、eventに詰める
   const handleSubmit = (event) => {
-    // こいつで本来のform送信の処理をキャンセルしている
-    // つまり今回はresutfullで通信形態がjsonなので、デフォルトのform機能は使用しないため
+    // 本来のform送信の処理をキャンセルする
     event.preventDefault();
     // event.target、つまりフォームの内容をオブジェクトに変換する
     // FormDataオブジェクトはkeyが名前、valueが値のオブジェクト
     // 例、名前=key、恭兵=valueという形式でオブジェクトになってる
     const formData = new FormData(event.target);
 
-    // js形式にある程度まとめてるってことかな？？？？
+    // formDataをjs形式にまとめる
     const newStock = {
       name: formData.get('name'),
       price: parseInt(formData.get('price')),
@@ -62,7 +61,6 @@ function Fruits() {
     // 追加処理の関数を呼び出して、追加処理を行う！
     addStock(newStock);
   }
-
 
   // 在庫情報を追加する関数
   const addStock = (formData) => {
@@ -80,7 +78,6 @@ function Fruits() {
       .then(response => {
         // resoponseのtrue判定?がtrueだったら、フルーツデータを再取得して更新
         if (response.ok) {
-          // 在庫情報が正常に追加された場合、フルーツデータを再取得して更新する
           return fetchFruitData();
         } else {
           // 更新失敗時のメッセージ
@@ -94,29 +91,16 @@ function Fruits() {
       });
   }
 
-
-
   // -------------------------ここまで---------------------------
-
-
-
-
-
-
 
 
 
 
   // ----------------------------削除処理---------------------------------
 
+  // 削除ボタンがクリックされたときに発動する関数
   const handleDelete = (event) => {
-
-    event.preventDefault();
-
-    // buttonタグのnameを取得
-    console.log(event.target.name);
-    // buttonタグのvalueを取得
-    console.log(event.target.value);
+    // buttonタグのvalue = 各要素のidを取得する
     let id = event.target.value;
 
     // urlとidの組み合わせを作って情報をjson形式で取得する
@@ -125,12 +109,14 @@ function Fruits() {
         console.log(value);
         // idをもとに取り出したデータをjs形式でまとめる
         const newStock = {
+          // idがないと削除されないためここは必須
+          id: value.id,
           name: value.name,
           price: value.price,
           stock: value.stock
         };
 
-        // 追加処理の関数を呼び出して、追加処理を行う！
+        // 削除処理の関数を呼び出して、削除処理を行う！
         deleteStock(newStock);
       })
     })
@@ -147,18 +133,15 @@ function Fruits() {
     fetch('http://localhost:8080/fruits/delete', {
       // 通信形式の設定
       method: 'POST',
-      // ここを書かないとエラーになる
       headers: {
         'Content-Type': 'application/json'
       },
-      // json形式にフォームのデータを変換している
+      // データの変換
       body: JSON.stringify(formData)
     })
-      // こいつらをresponse関数に詰める
       .then(response => {
         // resoponseのtrue判定?がtrueだったら、フルーツデータを再取得して更新
         if (response.ok) {
-          // 在庫情報が正常に追加された場合、フルーツデータを再取得して更新する
           return fetchFruitData();
         } else {
           // 更新失敗時のメッセージ
@@ -175,10 +158,6 @@ function Fruits() {
   // -------------------------ここまで---------------------------
 
 
-
-
-
-
   // データベースの値の加工処理、map関数でテーブル形式で出力できるように加工
   const fruitData = data && data.map((item, index) => {
     return (<tr key={index}>
@@ -192,7 +171,7 @@ function Fruits() {
     </tr>);
   })
 
-
+  // html
   return (
     <div>
       <h3>フルーツ在庫情報</h3>
